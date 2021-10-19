be99ca34b48982776911e238db5dc0e74b45cbb6
Your Name <nguyenlong2002>
2021-10-19 22:32:55 +0700

    first commit

---
+++ CSDL_QLnhahang.sql
@@ -0,0 +1,92 @@
+﻿CREATE DATABASE CSDL_QLnhahang
+GO
+
+USE CSDL_QLnhahang
+GO
+
+
+
+--Tạo bảng NHANVIEN
+create table NHANVIEN
+(
+Ma_NV char (8) not null primary key,
+Ten_NV varchar (30),
+Diachi_NV varchar (50),
+DienThoai_NV int,
+ChucVu varchar (30),
+NgaySinh date
+)
+GO
+--Tạo bảng LUONG
+create table LUONG
+(
+Ma_L char (10) not null primary key,
+Ma_NV char (8),
+ThanhToan int,
+foreign Key (Ma_NV) references NHANVIEN ( Ma_NV)
+)
+Go
+--Tạo bảng MENU
+create table MENU
+(
+Ma_MN char (5) not null primary key,
+Ten_M varchar (30),
+DonGai int
+)
+GO
+--Tạo bảng KHACH
+create table KHACH
+(
+Ma_K char (8) not null primary key,
+Ten_K varchar (30),
+DiaChi_K varchar (50),
+DienThoai_K int,
+Ma_NV char (8) not null,
+Ma_MN char (5) not null,
+foreign key (Ma_NV) references NHANVIEN (Ma_NV),
+foreign key (Ma_MN) references MENU ( Ma_MN)
+)
+GO
+--Tạo bảng HOADON
+create table HOADON
+(
+Ma_HD char (5) not null primary key,
+Ngay date,
+T_tien varchar (50),
+Ma_NV char (8) not null,
+Ma_K char (8) not null,
+foreign key (Ma_NV) references NHANVIEN (Ma_NV),
+foreign key (Ma_K) references KHACH (Ma_K)
+)
+Go
+--Tạo bảng KHACH
+create table THUCPHAM
+(
+Ma_TP char (8) not null primary key,
+Ten_TP varchar (30),
+Donvi_TP char (10),
+Ngay_N date,--ngày nhập
+Ngay_HH date,--ngày hết hạn
+Ma_NCC char (8) not null,
+foreign key (Ma_NCC) references NHA_CC (Ma_NCC)
+)
+GO
+--Tạo bảng NHACC
+create table NHA_CC
+(
+Ma_NCC char (8) not null primary key,
+Ten_NC varchar (30),
+Dia_C varchar (30),
+DienThoai_NCC int, 
+Nam_HT date ,     --Năm hợp tác
+Ma_NV char (8) not null,
+Ma_TP char (8) not null,
+foreign key (Ma_NV) references NHANVIEN (Ma_NV),
+foreign key (Ma_TP) references THUCPHAM(Ma_TP)
+)
+Go
+
+
+
+
+
---
+++ QLnhanvien.sql
@@ -0,0 +1,188 @@
+﻿CREATE DATABASE QLNhanVien
+go
+
+USE QLNhanVien
+GO
+--nhân viên
+--đơn vị
+--bảng lương
+create table DONVI
+(
+MaDV nvarchar(4) not null primary key,
+TenDV nvarchar(50)
+)
+go
+create table NHANVIEN
+(
+MaNV nvarchar(8) not null primary key,
+hoten nvarchar(30) not null,
+congviec nvarchar(50),
+luong int,
+MaDV nvarchar(4) not null,
+MaPT nvarchar(8)
+FOREIGN KEY(MaDV) REFERENCES DONVI(MaDV)
+)
+go
+
+create table BACLUONG
+(
+Maban nvarchar(50) not null primary key,
+Baccao int,
+bacthap int,
+)
+go
+INSERT dbo.DONVI
+(
+MaDV,
+TenDV)
+VALUES (N'0001',N'KHTN'),
+(N'0002',N'DHTL'),
+(N'0003',N'DHQL')
+go
+select *from donvi
+INSERT INTO NHANVIEN
+
+VALUES
+(
+	N'NV001',
+	N'Nguyễn Thành Long',
+	N'Giám Đốc',
+	1000,
+	N'0001',
+	N''
+),
+(	N'NV002',
+	N'Nguyễn Văn Thành',
+	N'Phó Giám Đốc',
+	800,
+	N'0001',
+	N''
+),
+(	N'NV003',
+	N'Nguyễn Thị Mai',
+	N'Giáo viên',
+	300,
+	N'0002',
+	N''
+),
+(	N'NV004',
+	N'Nguyễn Thu Trang',
+	N'Giáo viên',
+	400,
+	N'0002',
+	N''
+),
+(	N'NV005',
+	N'Mai Văn Thông',
+	N'Giáo viên',
+	100,
+	N'0003',
+	N''
+)
+
+SELECT*from NHANVIEN --Câu 6:Đưa ra thông tin của nhân viên
+INSERT dbo.BACLUONG
+(
+Maban ,
+bacthap,
+baccao
+)
+VALUES
+(
+	N'1',
+	400,
+	500
+),
+(
+	N'2',
+	501,
+	600
+),
+(
+	N'3',
+	601,
+	800
+)
+
+SELECT*From BACLUONG --Đưa ra thông tin của bảng lương
+
+SELECT hoten,congviec,luong From[NHANVIEN]--Câu 7
+
+SELECT Distinct congviec From NHANVIEN--Câu 8
+
+SELECT HoTen,Luong*3 FROM dbo.NHANVIEN --Câu 9
+
+SELECT HoTen,Luong from NHANVIEN--Câu 10:Sắp xếp lương tăng dần
+ORDER BY luong ASC
+SELECT HoTen,Luong from NHANVIEN --Sắp xếp lương giảm dần
+ORDER BY luong DESC
+
+SELECT HoTen,Luong from NHANVIEN--Câu 11
+WHERE Luong>300
+
+SELECT HoTen,Luong FROM dbo.NHANVIEN-- Câu 12
+where CongViec = 'Giáo Viên' and Luong>300
+
+SELECT HoTen FROM dbo.NHANVIEN--Câu 13
+Where luong=200 or luong=300 or luong=600
+
+SELECT HoTen FROM dbo.NHANVIEN--Câu 14
+Where 300 < luong and luong<1000
+
+SELECT hoten as"NV có họ Nguyễn",congviec from nhanvien--Câu 15
+where hoten like N'Nguyễn%'
+
+SELECT max(luong) as"Lương lớn nhất " from NHANVIEN --Câu 16
+SELECT min(luong) as"Lương nhỏ nhất"from NHANVIEN
+SELECT AVG(luong) as"Lương trung bình"FROM NHANVIEN
+
+SELECT congviec,AVG(luong) as"Lương TB của CV" --Câu 17
+FROM NHANVIEN 
+GROUP BY congviec
+
+SELECT congviec,AVG(luong) as"Lương TB của CV" --Câu 18
+From NHANVIEN
+where luong>200
+GROUP BY congviec
+
+Select MaDV, congviec, sum(luong) as"Tổng lương"--Câu 19:Tổng lương của từng công việc 
+from nhanvien										-----trong từng đơn vị.
+group by congviec,MaDV
+
+Select congviec ,AVG(luong) as"Lương TB" --Câu 20
+from nhanvien
+where luong>=300
+group by congviec
+
+Select TenDV,max(luong) as"Lương lớn nhất"--Câu 21 
+from DONVI,NHANVIEN
+where (Donvi.MaDV=nhanvien.MaDV) and luong>=300
+group by TenDV
+
+
+Select hoten,congviec,TenDV --Câu 22
+from nhanvien,donvi
+where (Donvi.MaDV=nhanvien.MaDV)
+
+SELECT HoTen, CongViec,TenDV--Câu 23:KQ chạy giống câu 22
+FROM NHANVIEN NV, DONVI DV
+WHERE NV.MaDV= DV.MaDV;
+
+Select HoTen, Congviec, TenDV --Câu 24
+FROM NHANVIEN NV, DONVI DV
+WHERE NV.MaDV= DV.MaDV and luong>=500
+
+SELECT NV.MaNV, NV.Hoten, PT.MaNV, PT.Hoten--Câu 25
+FROM NHANVIEN NV, NHANVIEN PT
+WHERE (NV.MaNV=PT.MaPT) AND (NV.Luong>PT.Luong);
+
+Select congviec from nhanvien --Câu 27
+where Madv like N'0001%' or Madv like N'0002%'
+
+
+Select hoten, TenDV, congviec,luong --Câu 28
+from nhanvien nv, donvi dv 
+where(nv.madv=dv.madv) and( luong > (select avg(luong) from nhanvien))
+
+Select hoten from nhanvien 
+where (luong > (select max(luong) from nhanvien,DONVI)) (nhanvien.madv=donvi.madv) and (tendv=N'DHTL'))
